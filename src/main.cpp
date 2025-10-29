e38c64957dc46e84109a1b7fb342c2bc9a76899e
Eilif Swensen <eilifswe@gmail.com>
2025-10-29 08:54:18 +0100

    Set up a simple window

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return 1;
+	}
+
+	if (!glfwVulkanSupported())
+	{
+		std::cerr << "GLFW with Vulkan is not supported on this system" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	/* No OpenGL context */
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "EFVK", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
